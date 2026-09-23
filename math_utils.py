5d5f8e5e338748958250be4c01b01c87fcc5b169
nischalkarki2 <priyanischal777@gmial.com>
2026-09-23 09:26:11 +0530

    Feature: add math utils function

---
+++ math_utils.py
@@ -0,0 +1,14 @@
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+ch = input("Enter choice: +, -, *, /: ")
+
+if (ch == "+"):
+    print(f"{a} + {b} = {a+b}")
+elif (ch == "-"):
+    print(f"{a} - {b} = {a-b}")
+elif (ch == "+"):
+    print(f"{a} * {b} = {a*b}")
+elif (ch == "+"):
+    print(f"{a} / {b} = {a/b}")
+else:
+    print("Invalid choice")
